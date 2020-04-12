3631ea2c514b8e3c1c97b6a80fa38a954c1191d8
Fahrenheit <jordan.silvaa42@gmail.com>
2020-04-12 04:09:41 -0300

    Removed extra assets, fixed VS Code and UI Boxes Preview

--- Sokotri/Assets/Box.cs
+++ Sokotri/Assets/Box.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    public enum type{
+        ground, //brown
+        fire, // red
+        water, // blue
+        grass, //green
+        steel, //gray
+        quintessential //special 
+
+    }
+
+    public static Color32 quintessentialColor = Color.magenta;
+
+    //probability of each type of box
+    static float[] typeWeights = {18,18,18,18,18,10};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +30,16 @@ public class Box : MonoBehaviour
     {
         
     }
+
+     public static Box.type  getRandomBoxType()
+    {
+        //get a random box based on their probabilities
+        int i = UtilityTools.GetRandomWeightedIndex(typeWeights);
+        //if by any chance the random doesn't work, random again but without the special one
+        if(i < 0) i = Random.Range(0, System.Enum.GetNames(typeof(Box.type)).Length - 1);
+
+        return (Box.type) i;
+
+    }
+
 }
---
+++ Sokotri/Assets/PreviewBoxes.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PreviewBoxes : MonoBehaviour
+{
+   [SerializeField]
+    Sprite [] sprites =  new Sprite[6];
+    SpriteRenderer spriteRenderer;    // Start is called before the first frame update
+
+    Box.type [] boxes = new Box.type[3];
+
+    [SerializeField]
+    GameObject[] boxesRenderers = new GameObject[3];
+    void Start()
+    {
+        if(spriteRenderer == null)
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        
+        generateNextPreview();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void updateSprites()
+    {
+        for(int i = 0; i < boxesRenderers.Length; i++)
+        {
+            if((int)boxes[i] < 0 || (int)boxes[i] >= sprites.Length || i < 0 || i >= boxes.Length) continue;
+
+            boxesRenderers[i].GetComponent<Image>().sprite = sprites[(int)boxes[i]];
+            boxesRenderers[i].GetComponent<Image>().color = (boxes[i] == Box.type.quintessential) ? Box.quintessentialColor : (Color32)Color.white;
+
+        }
+    }
+
+    void generateNextPreview()
+    {
+      for (int i = 0; i < boxes.Length; i++)
+      {
+          boxes[i] = Box.getRandomBoxType();
+      }
+
+      updateSprites();
+
+    }
+   
+}
